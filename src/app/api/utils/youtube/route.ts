b80ae3a0a0670b40f18c02947bcdc8e9fb4cef4a
Flory <flory.contato@gmail.com>
2025-03-03 14:15:10 -0300

    retirar o import do youtube api route

--- src/app/api/utils/youtube/route.ts
+++ src/app/api/utils/youtube/route.ts
@@ -1,6 +1,5 @@
-import { NextRequest, NextResponse } from "next/server";
 import { YoutubeTranscript } from "youtube-transcript";
-export async function GET(req: NextRequest, res: NextResponse) {
+export async function GET() {
   try {
     const response = await YoutubeTranscript.fetchTranscript(
       "https://www.youtube.com/watch?v=H4j1A_pY63U"
